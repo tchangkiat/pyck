3281898e5cc17dc1e67fda6ea1613d15bef06e3d
Tan Chang Kiat <tanchangkiat@gmail.com>
2024-04-09 16:25:00 +0000

    Refactored code in taskmanager

--- src/pyck/helpers/taskmanager.py
+++ src/pyck/helpers/taskmanager.py
@@ -1,26 +1,5 @@
 from alive_progress import alive_bar
 from concurrent.futures import ThreadPoolExecutor, as_completed
-import sys
-import traceback
-
-from pyck.helpers import logging
-
-
-class ThreadPoolExecutorStackTraced(ThreadPoolExecutor):
-    def submit(self, fn, *args, **kwargs):
-        """Submits the wrapped function instead of `fn`"""
-        return super(ThreadPoolExecutorStackTraced, self).submit(
-            self._function_wrapper, fn, *args, **kwargs
-        )
-
-    def _function_wrapper(self, fn, *args, **kwargs):
-        """Wraps `fn` in order to preserve the traceback of any kind of
-        raised exception"""
-        log = logging.Logging.get_instance()
-        try:
-            return fn(*args, **kwargs)
-        except Exception as e:
-            log.error(sys.exc_info()[0](traceback.format_exc()))
 
 
 class TaskManager:
@@ -28,38 +7,41 @@ class TaskManager:
         """Initiate a Task Manager."""
         self.tasks = []
 
-    def add_task(self, function, parameters: list = ""):
+    def add_task(self, function, parameters: list = None):
         """Add task to run later."""
         self.tasks.append({"function": function, "parameters": parameters})
 
     def run_tasks(self, description: str = "", wait: bool = True):
         """Run tasks that are added"""
         results = []
-        executor = ThreadPoolExecutorStackTraced(max_workers=10)
+        exceptions = []
+        executor = ThreadPoolExecutor(max_workers=10)
         futures = []
         if description:
             print(description)
         for task in self.tasks:
-            if task["parameters"] and type(task["parameters"]) is not list:
-                futures = [executor.submit(task["function"], task["parameters"])]
-            elif type(task["parameters"]) is list:
-                futures = [
+            if type(task["parameters"]) is list:
+                futures = futures + [
                     executor.submit(task["function"], parameter)
                     for parameter in task["parameters"]
                 ]
             else:
-                futures = [executor.submit(task["function"])]
+                if task["parameters"] is not None:
+                    futures = futures + [
+                        executor.submit(task["function"], task["parameters"])
+                    ]
+                else:
+                    futures = futures + [executor.submit(task["function"])]
         if wait:
             with alive_bar(len(futures)) as bar:
                 for future in as_completed(futures, timeout=10):
-                    exception = future.exception()
-                    if not exception:
+                    try:
                         results.append(future.result())
-                    else:
-                        results.append(exception)
+                    except:
+                        exceptions.append(future.exception())
                     bar()
             executor.shutdown()
         else:
             executor.shutdown(wait=False)
         self.tasks = []  # Clear tasks
-        return results
+        return results, exceptions
--- src/pyck/test/test.py
+++ src/pyck/test/test.py
@@ -1,4 +1,5 @@
 import time
+import traceback
 
 from pyck.helpers.logging import Logging
 from pyck.helpers.taskmanager import TaskManager
@@ -11,20 +12,27 @@ def test_task(random_text):
 
 
 def test_task_exception():
+    print("test_task_exception")
     raise Exception("Test Exception")
 
 
 log = Logging.get_instance()
 log.info("This is an info message")
 log.debug("This is a debug message")
-log.error("This is an error message", "Additional message")
-log.info(purple("This is a purple text"))
-
 tm = TaskManager()
 tm.add_task(test_task, parameters=["hello 1", "hello 2", "hello 3"])
 tm.run_tasks("Test Task Manager - Don't Wait", wait=False)
 tm2 = TaskManager()
 tm2.add_task(test_task_exception)
 tm2.add_task(test_task, parameters=["hello 4"])
-results = tm2.run_tasks("Test Task Manager - Exception")
+results, exceptions = tm2.run_tasks("Test Task Manager 2 - Exception")
 print(results)
+print(exceptions)
+log.error(
+    "".join(
+        traceback.format_exception(
+            type(exceptions[0]), value=exceptions[0], tb=exceptions[0].__traceback__
+        )
+    ),
+    "This exception is intentional - it tests the exception catching in concurrent.futures and logging of an error message.",
+)
