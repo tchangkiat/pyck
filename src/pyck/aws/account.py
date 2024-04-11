cc4bd15c152d0054897876ac451002cae1e8b5e1
Tan Chang Kiat <tanchangkiat@gmail.com>
2024-04-11 09:06:17 +0000

    Improved the error handling of AWS account and organizations helpers

--- src/pyck/aws/account.py
+++ src/pyck/aws/account.py
@@ -1,4 +1,7 @@
 import boto3
+from botocore.exceptions import ProfileNotFound, ClientError
+
+from pyck.helpers.logging import Logging
 
 
 class AwsAccount:
@@ -18,12 +21,25 @@ class AwsAccount:
     @classmethod
     def from_profile(cls, profile: str = "default"):
         """Create an AwsAccount object using credentials of AWS CLI profile"""
-        if profile:
-            session = boto3.Session(profile_name=profile)
-        else:
-            session = boto3.Session()
-        credentials = session.get_credentials().get_frozen_credentials()
-        return cls(credentials.access_key, credentials.secret_key, credentials.token)
+        try:
+            if profile:
+                session = boto3.Session(profile_name=profile)
+            else:
+                session = boto3.Session()
+            credentials = session.get_credentials().get_frozen_credentials()
+            return cls(
+                credentials.access_key, credentials.secret_key, credentials.token
+            )
+        except ProfileNotFound as e:
+            log = Logging.get_instance()
+            log.error(
+                "Unable to find the AWS CLI profile '"
+                + e.__dict__["kwargs"]["profile"]
+                + "'. ",
+                resources=[
+                    "Configure a profile for AWS CLI: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-quickstart.html"
+                ],
+            )
 
     @classmethod
     def from_assuming_role(
@@ -33,15 +49,30 @@ class AwsAccount:
         profile_to_assume_role: str = "default",
     ):
         """Create an AwsAccount object based on assumed role"""
-        aws_account = cls.from_profile(profile_to_assume_role)
-        client = aws_account.session.client("sts")
-        response = client.assume_role(
-            RoleArn="arn:aws:iam::" + account_id + ":role/" + role_name,
-            RoleSessionName=account_id + "-" + role_name,
-        )
-        credentials = response["Credentials"]
-        return cls(
-            credentials["AccessKeyId"],
-            credentials["SecretAccessKey"],
-            credentials["SessionToken"],
-        )
+        try:
+            aws_account = cls.from_profile(profile_to_assume_role)
+            client = aws_account.session.client("sts")
+            response = client.assume_role(
+                RoleArn="arn:aws:iam::" + account_id + ":role/" + role_name,
+                RoleSessionName=account_id + "-" + role_name,
+            )
+            credentials = response["Credentials"]
+            return cls(
+                credentials["AccessKeyId"],
+                credentials["SecretAccessKey"],
+                credentials["SessionToken"],
+            )
+        except ClientError as e:
+            log = Logging.get_instance()
+            if "(AccessDenied) when calling the AssumeRole operation" in str(e):
+                log.error(
+                    "Unable to assume role '"
+                    + "arn:aws:iam::"
+                    + account_id
+                    + ":role/"
+                    + role_name
+                    + "'. Ensure that the profile has permission to perform 'sts:AssumeRole', the role exists, and the management account is added as a trust entity in the role.",
+                    resources=[
+                        "Use trust policies with IAM roles: https://aws.amazon.com/blogs/security/how-to-use-trust-policies-with-iam-roles/"
+                    ],
+                )
--- src/pyck/aws/organizations.py
+++ src/pyck/aws/organizations.py
@@ -1,5 +1,6 @@
 from pyck.aws.account import AwsAccount
 from pyck.aws.service import Service
+from pyck.helpers.logging import Logging
 
 
 class Organizations(Service):
@@ -9,9 +10,19 @@ class Organizations(Service):
 
     def get_accounts(self):
         """Get accounts in AWS Organizations"""
-        result = super().get(self.client, "list_accounts", "Accounts")
-        return [
-            {"Id": o["Id"], "Name": o["Name"]}
-            for o in result
-            if o["Status"] == "ACTIVE"
-        ]
+        try:
+            result = super().get(self.client, "list_accounts", "Accounts")
+            return [
+                {"Id": o["Id"], "Name": o["Name"]}
+                for o in result
+                if o["Status"] == "ACTIVE"
+            ]
+        except Exception as e:
+            log = Logging.get_instance()
+            if (
+                "(AccessDeniedException) when calling the ListAccounts operation"
+                in str(e)
+            ):
+                log.error(
+                    "Unable to retrieve the list of accounts in the organization. Please check if the profile has the 'organizations:ListAccounts' permission"
+                )
