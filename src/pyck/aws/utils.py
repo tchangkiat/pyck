da9ac185a927062d66f0c087b3b83b280d4324a1
Tan Chang Kiat <tanchangkiat@gmail.com>
2024-04-25 08:48:27 +0000

    Added AWS CLI profile parameter to organization map function

--- src/pyck/aws/utils.py
+++ src/pyck/aws/utils.py
@@ -4,16 +4,22 @@ from pyck.helpers.taskmanager import TaskManager
 
 
 def organization_map(
-    aws_account: AwsAccount, role_to_assume: str, func, parameters: list
+    payer_account: AwsAccount,
+    role_to_assume: str,
+    func,
+    parameters: list,
+    aws_cli_profile: str = "default",
 ) -> tuple[list[any], list[Exception]]:
-    "Loop through an AWS organization and invoke a function for each account in the organization. The function needs to accept 2 parameters of type 'AwsAccount' and 'list' respectively"
-    org = Organization(aws_account)
+    "Loop through an AWS organization and invoke a function for each account in the organization. The invoked function needs to accept 2 parameters of type 'AwsAccount' and 'list' respectively"
+    org = Organization(payer_account)
     accounts = org.get_accounts()
     results = []
     tm = TaskManager()
 
     for account in accounts:
-        aws_account = AwsAccount.from_assuming_role(account["id"], role_to_assume)
+        aws_account = AwsAccount.from_assuming_role(
+            account["id"], role_to_assume, aws_cli_profile
+        )
         if aws_account is not None:
             tm.add_task(func, parameters=[aws_account] + parameters)
     results, exceptions = tm.run_tasks()
