37880a11d40fbd185367d80a4dbc7590f57c75ea
Tan Chang Kiat <tanchangkiat@gmail.com>
2024-04-26 02:44:38 +0000

    Removed the use of Task Manager in AWS organization map function

--- src/pyck/aws/utils.py
+++ src/pyck/aws/utils.py
@@ -1,6 +1,5 @@
 from pyck.aws.account import AwsAccount
 from pyck.aws.organizations import Organization
-from pyck.helpers.taskmanager import TaskManager
 
 
 def organization_map(
@@ -14,13 +13,10 @@ def organization_map(
     org = Organization(payer_account)
     accounts = org.get_accounts()
     results = []
-    tm = TaskManager()
 
     for account in accounts:
         aws_account = AwsAccount.from_assuming_role(
             account["id"], role_to_assume, aws_cli_profile
         )
-        if aws_account is not None:
-            tm.add_task(func, parameters=[aws_account] + parameters)
-    results, exceptions = tm.run_tasks()
-    return results, exceptions
+        results.append(func(aws_account, *parameters))
+    return results
