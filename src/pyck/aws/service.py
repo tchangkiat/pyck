440b46a2fcb5a2e2058851b074346a66f9af09b5
Tan Chang Kiat <tanchangkiat@gmail.com>
2025-04-28 09:07:07 +0000

    Allow additional arguments

--- src/pyck/aws/service.py
+++ src/pyck/aws/service.py
@@ -1,8 +1,8 @@
 class Service:
-    def get(self, client, operation, key):
+    def get(self, client, operation, key, **kwargs):
         """Get results with paginator"""
         paginator = client.get_paginator(operation)
-        page_iterator = paginator.paginate()
+        page_iterator = paginator.paginate(**kwargs)
         results = []
         for page in page_iterator:
             results += page[key]
